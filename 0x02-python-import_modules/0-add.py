151cb73b51c7f2d07e5a8863838be758bec14aef
Delsa Marasha <deemarasha77@gmail.com>
2023-08-16 10:33:46 -0700

    fixing 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-import add_0
-
 a = 1
 b = 2
 
 if __name__ == "__main__":
-result = add_0.add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, result))
+    from add_0 import add
+
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
