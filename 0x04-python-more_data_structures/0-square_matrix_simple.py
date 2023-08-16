4ba4216f32674d75db1d2f75cef23387ac2b7696
Delsa Marasha <deemarasha77@gmail.com>
2023-08-16 12:29:48 -0700

    1-search_replace.py and 0-square_matrix_simple.py update 0.2

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,5 @@
-def square_matrix_simple(matrix=[]):
-    new_matrix = [[num * num for num in row] for row in matrix]
-    return new_matrix
+#!/usr/bin/python3
 
-# Test the function with the provided example
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
+def square_matrix_simple(matrix=[]):
+    new_matrix = [[num ** 2 for num in row] for row in matrix]
+    return new_matrix
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,5 @@
+#!/usr/bin/python3
+
 def search_replace(my_list, search, replace):
     new_list = [replace if num == search else num for num in my_list]
     return new_list
-
-# Test the function with the provided example
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-print(new_list)
-print(my_list)
