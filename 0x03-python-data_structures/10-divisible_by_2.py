67ec441f8a040fd215896d9b231f88de2bb38a94
Delsa Marasha <deemarasha77@gmail.com>
2023-08-18 22:19:38 -0700

    11. Delete at

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     my_list = [0, 1, 2, 3, 4, 5, 6]
     list_result = divisible_by_2(my_list)
 
-    i = 0
-    while i < len(list_result):
-        print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-        i += 1
+    for num, result in zip(my_list, list_result):
+        status = "is" if result else "is not"
+        print("{:d} {:s} divisible by 2".format(num, status))
