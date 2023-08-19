6473103c8e75b5675b020e1bc22f6e5aea98ee5a
Delsa Marasha <deemarasha77@gmail.com>
2023-08-18 21:49:52 -0700

    7. Tuples addition

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -7,6 +7,7 @@ def replace_in_list(my_list, idx, element):
 def print_reversed_list_integer(my_list=[]):
 def new_in_list(my_list, idx, element):
 def no_c(my_string):
-def print_matrix_integer(matrix=[[]]):	
+def print_matrix_integer(matrix=[[]]):
+def add_tuple(tuple_a=(), tuple_b=()):
 
 #endif /* LISTS_H */
