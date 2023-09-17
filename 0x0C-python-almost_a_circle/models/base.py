f23d656d525cd307eebbd12b2ac183fb529c686e
Delsa Marasha <deemarasha77@gmail.com>
2023-09-17 09:26:27 -0700

    0x0C. Python - Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+class Base:
+    __nb_objects = 0  # Class variable to track the next available ID
+
+    def __init__(self, value=None):
+        if value is not None:
+            self.id = value
+        else:
+            Base.__nb_objects += 1  # Increment the ID counter
+            self.id = Base.__nb_objects  # Assign the incremented ID
+
+    def display(self):
+        print(f"Value: {self.id}")
+
+
+if __name__ == "__main__":
+    # This code will execute if you run base.py directly
+    base_instance = Base(42)
+    base_instance.display()
