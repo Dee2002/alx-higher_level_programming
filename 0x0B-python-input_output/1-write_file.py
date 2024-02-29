f63364ad5c744c1e391325972c2ffaa91de77c2d
Delsa Marasha <deemarasha77@gmail.com>
2024-02-28 16:10:11 -0800

    Repo fix

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def write_file(filename="", text=""):
+        """Writes a string to a text file and returns the number of characters written."""
+            with open(filename, 'w', encoding='utf-8') as file:
+                        return file.write(text)
+
+                    # Test
+                    if __name__ == "__main__":
+                            nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+                                print(nb_characters)
+
